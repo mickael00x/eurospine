a0d3cf944043c2f42bf3e5c8e1b19beafa4ff57f
Guillaume <github@id2.fr>
2022-01-19 10:54:15 +0100

    CSS, homepage image, WIP template email

--- front-page.php
+++ front-page.php
@@ -13,19 +13,16 @@
                 <label class="label-homepage" for="input-homepage-no">No</label>
             </div>
         </div>
-        
+
         <div class="inputs-validation">
             <input type="text" id="input-validation" placeholder="Enter member discount code here  " name="input-validation">
             <button type="button" id="input-button">Submit</button>
         </div>
         <div class="inputs-validation-infos">
-            <p>
-            You are eligible to receive a discount code if you are members in good standing of EUROSPINE. <br>
-            In other words, you must have paid the EUROSPINE membership fees for 2022.  <br>
-            <p>Membership fees can be paid online directly in your EUROSPINE <a href="https://www.m-anage.com/Home/Index/Society/EUROSPINE-MB/en-GB">membership account: </a> </p>
-            If you have paid your 2022 membership fees and still have not received your discount code by email, please contact: <a href="mailto:membership@eurospine.org">membership@eurospine.org</a>
-            </p>
+            <p>Please note that, if you already are a EUROSPINE member and wish to take advantage of the EUROSPINE member discount when registering for the course, you must ensure you have settled your 2022 membership fee before starting your course registration. You can do this in your <a href="https://www.m-anage.com/Home/Index/Society/EUROSPINE-MB/en-GB">membership account</a>.</p>
+            <p>Once your membership payment has been cleared by our accountant, the member discount code will be shared with you in your M-anage account under the tile “Resources”.</p>
+            <p>Please note that we cannot reimburse the difference between the non-member and members registration fee if a member registers at the non-members rate.</p>
+            <p>If you have paid your 2022 membership fees and still have not received your discount code by email, please contact: <a href="mailto:membership@eurospine.org">membership@eurospine.org</a></p>
         </div>
     </div>
 </main>
-
